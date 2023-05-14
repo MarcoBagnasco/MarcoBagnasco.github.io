667959d15da745e2d341030873ed7402ed67ddaa
MarcoBagnasco <bagna.hbk@gmail.com>
2023-05-14 15:36:07 +0200

    finish copy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
+import logo from './assets/logo_bianco.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="container">
-      <img className="img" src="./assets/logo_bianoc.png" alt="logo"/>
+      <img className="img" src={logo} alt="logo"/>
       <h1>Coming Soon.....</h1>
     </div>
   )
